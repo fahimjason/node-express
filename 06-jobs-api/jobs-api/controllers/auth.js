a895bf400f2794bf79cce56544d1dc196f533e60
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-07-21 22:02:49 +0600

    Setup create user basic

--- 06-jobs-api/jobs-api/controllers/auth.js
+++ 06-jobs-api/jobs-api/controllers/auth.js
@@ -1,5 +1,17 @@
-const register = (req, res) => {
-    res.send('Register user');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
+const User = require('../models/User');
+
+const register = async (req, res) => {
+    // const { name, email, password } = req.body;
+
+    // if (!name || !email || !password) {
+    //     throw new BadRequestError('Please provide name, email and password');
+    // }
+
+    const user = await User.create({ ...req.body });
+
+    res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = (req, res) => {
