3e06b2db3ad6f4d1172bcce0d31a5c5174b80a52
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-07-24 23:31:47 +0600

    Implement user login

--- 06-jobs-api/jobs-api/controllers/auth.js
+++ 06-jobs-api/jobs-api/controllers/auth.js
@@ -1,5 +1,5 @@
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError } = require('../errors');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 const User = require('../models/User');
 
 const register = async (req, res) => {
@@ -15,8 +15,29 @@ const register = async (req, res) => {
     res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
-const login = (req, res) => {
-    res.send('Login user');
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        throw new BadRequestError('Please provide email and password');
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new UnauthenticatedError('Invalid Credentials');
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password);
+
+    if (!isPasswordCorrect) {
+        throw new UnauthenticatedError('Invalid Credentials');
+    }
+
+    // compare password
+    const token = user.createJWT();
+
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
