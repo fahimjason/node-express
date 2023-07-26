6481e00bc814d1ff96a5bbfd60c62eabfd6608dd
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-07-26 23:53:55 +0600

    Implement create job and get all jobs

--- 06-jobs-api/jobs-api/controllers/jobs.js
+++ 06-jobs-api/jobs-api/controllers/jobs.js
@@ -1,13 +1,22 @@
-const getAllJobs = (req, res) => {
-    res.send('Get jobs');
+const Job = require('../models/Job');
+const { StatusCodes } = require('http-status-codes');
+
+const getAllJobs = async (req, res) => {
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt');
+
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = (req, res) => {
     res.send('Get job');
 };
 
-const createJob = (req, res) => {
-    res.send('Create job');
+const createJob = async (req, res) => {
+    req.body.createdBy = req.user.userId;
+
+    const job = await Job.create(req.body);
+
+    res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = (req, res) => {
--- 06-jobs-api/jobs-api/middleware/error-handler.js
+++ 06-jobs-api/jobs-api/middleware/error-handler.js
@@ -4,7 +4,9 @@ const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
     if (err instanceof CustomAPIError) {
         return res.status(err.statusCode).json({ msg: err.message });
+        console.log(err);
     }
+    console.log(err);
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
 }
 
