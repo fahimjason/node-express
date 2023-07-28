d8e2c0223cc0e62b2d49fd978cacb6f4133a23f3
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-07-29 00:42:26 +0600

    Add swagger-api documentation and fix module not found error in job-api/controllers/jobs.js

--- 06-jobs-api/jobs-api/app.js
+++ 06-jobs-api/jobs-api/app.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 require('express-async-errors');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 // extra security packages
 const helmet = require('helmet');
 const cors = require('cors');
@@ -33,6 +38,11 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get('/', (req, res) => {
+    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
