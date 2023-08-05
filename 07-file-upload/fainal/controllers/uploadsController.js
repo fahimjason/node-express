74c1f035c408ca46515a29a842b9f939f3db6eaa
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-08-05 23:41:08 +0600

    Implement image upload with cloudinary

--- 07-file-upload/fainal/controllers/uploadsController.js
+++ 07-file-upload/fainal/controllers/uploadsController.js
@@ -4,7 +4,7 @@ const CustomError = require('../errors');
 const cloudinary = require('cloudinary').v2;
 const fs = require('fs');
 
-const uploadProductImage = async (req, res) => {
+const uploadProductImageLocal = async (req, res) => {
     if (!req.files) {
         throw new CustomError.BadRequestError('No File Uploaded');
     }
@@ -24,6 +24,20 @@ const uploadProductImage = async (req, res) => {
     return res
         .status(StatusCodes.OK)
         .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+const uploadProductImage = async (req, res) => {
+    const result = await cloudinary.uploader.upload(
+        req.files.image.tempFilePath,
+        {
+            use_filename: true,
+            folder: 'file-upload',
+        }
+    );
+
+    fs.unlinkSync(req.files.image.tempFilePath);
+
+    return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
 };
 
 module.exports = {
