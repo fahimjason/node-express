e106c4f0eb671e076fdc6c174f1d6086808684f8
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2023-08-08 00:44:08 +0600

    Send email with send-grid

--- 08-send-email/final/controllers/sendEmail.js
+++ 08-send-email/final/controllers/sendEmail.js
@@ -1,19 +1,20 @@
 const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 
-const sendEmail = async (req, res) => {
+const sendEmailEthereal = async (req, res) => {
     let testAccount = await nodemailer.createTestAccount();
 
     const transporter = nodemailer.createTransport({
         host: 'smtp.ethereal.email',
         port: 587,
         auth: {
-            user: 'marlene.legros@ethereal.email',
-            pass: 'va4q5BKKtry7aq58Gv',
+            user: 'vicente.erdman@ethereal.email',
+            pass: '1GB934anxDnS161SUZ',
         },
     });
 
     let info = await transporter.sendMail({
-        from: '"Coding Addict" <codingaddict@gmail.com>',
+        from: '"Coding Addict" <vicente@gmail.com>',
         to: 'bar@example.com',
         subject: 'Hello',
         html: '<h2>Sending Emails with Node.js</h2>',
@@ -22,4 +23,20 @@ const sendEmail = async (req, res) => {
     res.json(info);
 };
 
-module.export = sendEmail;
+const sendEmail = async (req, res) => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const msg = {
+        to: 'fahimulhoquejason@mail.com', // Change to your recipient
+        from: 'thefahimulhoque@gmail.com', // Change to your verified sender
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+
+    const info = await sgMail.send(msg);
+
+    res.json(info);
+};
+
+module.exports = sendEmail;
